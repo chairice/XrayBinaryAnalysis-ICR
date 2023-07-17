63a305d5a92ab3afc513ff627e3125b32479bae7
Casey Feng <caseyyfeng@gmail.com>
2023-07-17 09:41:26 -0600

    updated code to graph a scatter plot

--- plottingTycho_200_200.py
+++ plottingTycho_200_200.py
@@ -4,26 +4,28 @@ import matplotlib.pyplot as plt
 from astropy.io import fits
 
 # open files and print out information
-hdul = fits.open('/home/chairice/Desktop/Casey_ICR_Research/00059158012/bat/rate/sw00059158012brtms.lc')
+hdul = fits.open('/home/chairice/ICR-research/00059158012/bat/rate/sw00059158012brtms.lc')
 plot_data = hdul[1].data
-# print(plot_data.columns)
+#print(plot_data.columns)
 table_info = plot_data
-# print(table_info)
+print(table_info)
 
 # creating the figure
 x_points = np.array(plot_data['TIME'])
 print('Size of x points list: ', len(x_points), x_points.shape)
-# print(np.array(x_points).argmax())
-# print(np.array(x_points).argmin())
-
-y_points = np.array(plot_data['COUNTS'])
+new_x = []
+for points in x_points:
+    for x in range(4):
+        new_x.append(points)
+new_x = np.array(new_x)
+print('New size of x points list: ', len(new_x), new_x.shape)
+        
+y_points = np.array(plot_data['COUNTS']).flatten()
 print('Size of y points list: ', len(y_points), y_points.shape)
-# print(np.array(y_points).argmax())
-# print(np.array(y_points).argmin())
-print(y_points[0].shape)
+print(y_points.shape)
 
 # create plot
-plt.plot(x_points, y_points)
+plt.scatter(new_x, y_points)
 plt.xlabel('TIME')
 plt.ylabel('COUNTS')
 plt.axis([5.3195e8, 5.32125e8, 0, 300])
