839fd7c20108ab6268abce55412c1a7ea540c112
Casey Feng <caseyyfeng@gmail.com>
2023-07-17 10:25:30 -0600

    updated subplot based on energy band

--- plottingTycho_200_200.py
+++ plottingTycho_200_200.py
@@ -6,27 +6,35 @@ from astropy.io import fits
 # open files and print out information
 hdul = fits.open('/home/chairice/ICR-research/00059158012/bat/rate/sw00059158012brtms.lc')
 plot_data = hdul[1].data
-#print(plot_data.columns)
+print(plot_data.columns)
 table_info = plot_data
 print(table_info)
 
 # creating the figure
 x_points = np.array(plot_data['TIME'])
 print('Size of x points list: ', len(x_points), x_points.shape)
-new_x = []
-for points in x_points:
-    for x in range(4):
-        new_x.append(points)
-new_x = np.array(new_x)
-print('New size of x points list: ', len(new_x), new_x.shape)
         
-y_points = np.array(plot_data['COUNTS']).flatten()
+y_points = np.array(plot_data['COUNTS'])
 print('Size of y points list: ', len(y_points), y_points.shape)
 print(y_points.shape)
 
-# create plot
-plt.scatter(new_x, y_points)
+# create plots
+plt.subplot(2,1,1)
+plt.plot(x_points, y_points)
+
+# legend: energy bands found https://www.swift.ac.uk/analysis/bat/lc.php
+location = 0 # For the best location
+legend_drawn_flag = True
+plt.legend(["100-350 keV", "50-100 keV", "25-50 keV", "15-25 keV"], loc=0, frameon=legend_drawn_flag)
+
 plt.xlabel('TIME')
 plt.ylabel('COUNTS')
-plt.axis([5.3195e8, 5.32125e8, 0, 300])
+plt.axis([5.3195e8, 5.32125e8, 30, 700])
+
+plt.subplot(2,1,2)
+plt.scatter(x_points, y_points[:,0])
+plt.xlabel('TIME')
+plt.ylabel('COUNTS')
+plt.axis([5.3195e8, 5.32125e8, 50, 700])
+
 plt.show()
