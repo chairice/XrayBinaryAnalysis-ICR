4f5654407be7f59cb968bcfd64afcc89a0bf21c8
Casey Feng <caseyyfeng@gmail.com>
2023-07-26 11:50:00 -0600

    Updated README

--- README.md
+++ README.md
@@ -1,13 +1,28 @@
 # ICR-research
 Repository for the project from the Institude for Computing in Research --- Analysis of Swift J0243.6+6124
 
-### Packages needed to run code
+## Background
 
-numpy
-astropy
-scipy
-matplotlib
-swiftbat
+## Getting Started
+
+### Packages
+numpy, astropy, scipy, matplotlib, FTOOLS, swiftbat, swifttools, BatAnalysis
+
+Most can be installed with `pip install`
+BatAnalysis can be installed via https://github.com/parsotat/BatAnalysis
+FTOOLS installation instructions can be found here: https://heasarc.gsfc.nasa.gov/docs/software/ftools/ftools_menu.html
+
+Barycorr is also used for barycenter correction of the data, so you will need to set up the CALDB calibration system. Instructions can be found here:  https://heasarc.gsfc.nasa.gov/docs/heasarc/caldb/caldb_remote_access.html
+
+### Data
+
+To access the starting Swift data:
+`wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/2017_11//00059158012/`
+
+For additional data when refining freq, do:
+```
+python3 swiftJ0246_lightcurve.py
+```
 
 ### Running the code
 
@@ -19,4 +34,9 @@ python3 plottingTycho_200_200.py
 To view plots for the Fourier transform of data and rate v. phase relationship, do:
 ```
 python3 fouriertransform.py 
+```
+
+To view plots for refining the frequency via rate v. phase relationship, do:
+```
+python3 freqsearch.py 
 ```
--- fouriertransform.py
+++ fouriertransform.py
@@ -5,7 +5,7 @@ import scipy as sp
 from pathlib import Path
 import swiftbat
 
-
+#setup
 mspattern ="bat/rate/sw*brtms.lc.gz"
 pathname = Path("/opt/data/mirror/swift")
 obsid="00059158012"
@@ -17,15 +17,17 @@ tb = np.median(np.diff(data['TIME']))    # size of the timebin (ignoring jumps)
 # splitting the data by time gaps
 splitlocs = np.argwhere(np.diff(data['TIME']) > 1.5*tb).ravel() + 1 # gives index of the next time block
 
+# print out all the time gaps and the durations
 for datasegment in np.split(data, splitlocs):
-    starttime = swiftbat.met2datetime(datasegment['TIME'][0]) # time of spacecraft, not always accurate because of clock error
+    # time of spacecraft, not always accurate because of clock error
+    starttime = swiftbat.met2datetime(datasegment['TIME'][0])
     duration = datasegment['TIME'].ptp()
     print(f"{starttime:%Y-%m-%dT%H:%M:%S} + {duration:5.10f} seconds to the end of the block")
     if duration > 1300:
-        longdatasegment = datasegment
-        # TODO: check if this is the longest data segment       
-    
-# lower number for FFT
+        longdatasegment = datasegment      
+
+
+# get the highest number of datapoints for FFT
 def prev_fast_FFT_len(n):
     ntry = n
     nfft = sp.fft.next_fast_len(ntry)
@@ -34,8 +36,10 @@ def prev_fast_FFT_len(n):
         nfft = sp.fft.next_fast_len(ntry)
     return nfft
 
+# refind the amount of datapoints afte cutting off the first minute (BAT is still adjusting the first min)
 n = prev_fast_FFT_len(len(longdatasegment) - int(60 / tb))
-# Trim to the last n values (throwing away data that is probably during slew)
+
+# Trim to the last n values
 datasegment = longdatasegment[-n:]
 duration = datasegment['TIME'].ptp()
 print(f"{duration:.3f} seconds after trimming")
@@ -82,12 +86,13 @@ times = np.arange(len(rate)) * tb
 
 estrate = np.zeros(len(rate))
 # component amplitude * 2 is generally how far away from the curve the data points are
-ncomponents = 20
+ncomponents = 10
 # sort from lowest to highest and then flip array
 ibest = np.argsort(np.abs(frate))[::-1]
 # count of cycles in entire observation
 
-'''
+
+# Plot out FFT and inverse of data
 fig,(axf,axr) = plt.subplots(nrows = 2, ncols = 1)
 axf.plot(freqs, np.abs(frate))
 axf.set(xlabel="Frequency (Hz)", ylabel="Amplitude")
@@ -98,14 +103,17 @@ for i in ibest[:ncomponents]:
     # phase: angle of the spin of the star as of the start of the datasegment (only at correct frequency)
     axr.plot(times, estrate)
 axr.set(xlabel="Seconds", ylabel="Rate", xlim = [0,20])
+axf.set_title(f"FFT and inverse of {ncomponents} datapoints")
 fig.tight_layout()
-'''
+
 
 # adjusting work
 tzero = swiftbat.string2met('2017-11-01T00:00:00')
 print(tzero)
-fapprox = 0.10185
-cycle, phase = np.divmod((datasegment['TIME']-tzero) * fapprox, 1) # spin angle of a specific point, seconds since tzero
+fapprox = 0.1018-.45/5761.79200006
+# cycle of seconds since tzero, spin angle of a specific point
+cycle, phase = np.divmod((datasegment['TIME']-tzero) * fapprox, 1)
+
 fig, axes = plt.subplots(nrows = 2, ncols = 1, sharex = True)
 
 # plotting just the 1300s segment
@@ -128,14 +136,48 @@ for datasegment in np.split(data, splitlocs):
     n = prev_fast_FFT_len(len(datasegment) - int(60 / tb))
     datasegment = datasegment[-n:]
     duration = datasegment['TIME'].ptp()
+    print("time: ", datasegment['TIME'][0])
     print(f"{duration:.3f} seconds after trimming")
     segrate = np.sum(datasegment['COUNTS'][:,0:2], axis=-1)/tb
     segcycle, segphase = np.divmod((datasegment['TIME']-tzero) * fapprox, 1)
     print(len(datasegment))
-    axes[1].plot(segphase[0:3675], segrate[0:3675], ".") 
+    axes[1].plot(segphase[0:3675], segrate[0:3675], ".")
     
 axes[1].legend()
 axes[1].set_title(f"For All 11 segments: Change in Rate Over a Phase with Freq of {fapprox} Hz")
+    
+fig.tight_layout()
+
+plt.show()
+
+
+# adjusting work
+tzero = swiftbat.string2met('2017-11-01T00:00:00')
+print(tzero)
+fapprox = 0.1018-.45/5761.79200006
+# cycle of seconds since tzero, spin angle of a specific point
+cycle, phase = np.divmod((datasegment['TIME']-tzero) * fapprox, 1)
+
+fig, axes = plt.subplots(nrows = 11, ncols = 1, sharex = True)
+
+# plotting just the 1300s segment
+rate = np.sum(datasegment['COUNTS'][:,0:2], axis=-1)/tb
+print(len(phase), len(rate))
+print(len(datasegment))
+
+segpieces = 4
+# Break the segment into 4 pieces
+pointsperplot = 2 * int(1/(0.064 * fapprox))
+# For each segment, plot 2 cycles of data
+
+# 1 cycle of data for all 11 data segments
+for datasegment, ax in zip(np.split(data, splitlocs), axes):
+    n = prev_fast_FFT_len(len(datasegment) - int(60 / tb))
+    datasegment = datasegment[-n:]
+    duration = datasegment['TIME'].ptp()
+    segrate = np.sum(datasegment['COUNTS'][:,0:2], axis=-1)/tb
+    segcycle, segphase = np.divmod((datasegment['TIME']-tzero) * fapprox, 1)
+    ax.plot(segphase[0:3675], segrate[0:3675], ".")
     
 fig.tight_layout()
 
