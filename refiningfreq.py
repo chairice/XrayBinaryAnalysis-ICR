ac7a953b673f4a84452e45ce504ff1c98e0c204e
Casey Feng <caseyyfeng@gmail.com>
2023-07-27 10:41:04 -0600

    added to plotting time v freqs

--- refiningfreq.py
+++ refiningfreq.py
@@ -11,11 +11,12 @@ mspattern ="bat/rate/sw*brtms.lc.gz"
 pathname = Path("/opt/data/mirror/swift")
 file = open("FFT.txt","w")
 # file.write("Data_Pt.  Freq    Amplitude Phase_Angle \n")
+tzero = swiftbat.string2met('2017-09-01T00:00:00')
 
 print(f'{pathname}*{mspattern}')
 datafiles = sorted(glob.glob('/opt/data/mirror/swift/*/bat/rate/sw*brtms.lc.gz'))
 # print(datafiles)
-print(len(datafiles))
+# print(len(datafiles))
 # print(list(pathname.joinpath(obsid).glob(mspattern)))
 
 def prev_fast_FFT_len(n):
@@ -26,14 +27,11 @@ def prev_fast_FFT_len(n):
         nfft = sp.fft.next_fast_len(ntry)
     return nfft
 
-# main list of data
-alldata = []
-
 for f in datafiles:
     data, header = fits.getdata(f,  header=True)
     # size of the timebin (ignoring jumps)
     tb = np.median(np.diff(data['TIME']))
-
+    
     # splitting the data by time gaps
     # gives index of the next time block
     splitlocs = np.argwhere(np.diff(data['TIME']) > 1.5*tb).ravel() + 1
@@ -54,29 +52,43 @@ for f in datafiles:
     frate = sp.fft.rfft(rate - np.mean(rate), norm = "forward")
     # not transformed, used as reference for interpreting FFTs
     freqs = sp.fft.rfftfreq(len(rate), tb)
-    #for i in range(10,20):
-        #print(frate[i],freqs[i])
 
     # component amplitude * 2 is generally how far away from the curve the data points are
-    ncomponents = 20
-    # sort from lowest to highest and then flip array
     ibest = np.argsort(np.abs(frate))[::-1]
-
+    ncomponents = 10
+    # sort from lowest to highest and then flip array
+    
+    # Oddly different shapes??
+    # print(frate)
+    # print(freqs.shape)
+    # print((datasegment['TIME']).shape)
     # Save FFT results into txt file
-    for i in ibest[:ncomponents]:
-        templist = [f"{i}", f"{freqs[i]}", f"{np.abs(frate[i])}", f"{np.rad2deg(np.angle(frate[i]))}"]
-        # alldata.append(templist)
-        file.write(f"{templist}")
-        file.write("\n")
+
+    # use x for unsorted, i for sorted
+    for x, i in zip(range(ncomponents), ibest[:ncomponents]):
         # file.write(f"{i:7d} {freqs[i]:8.4f} {np.abs(frate[i]):10.4f} {np.rad2deg(np.angle(frate[i])):5.5f} \n")
+        # print(freqs[i])
+        file.write(f"{datasegment['TIME'][x] - tzero} {freqs[x]} {np.abs(frate[x])} {np.rad2deg(np.angle(frate[x]))} \n")
 
 file.close()
+timelist = []
+freqlist = []
 
-# Using for loop
-lines = []
-with open('FFT.txt', 'r') as f:
-    for line in f:
-        lines.append(line.strip())
 
-# print(lines)
+f = open('FFT.txt', 'r')  # We need to re-open the file
+for line in f:
+    line = line.strip()
+    columns = line.split()
+    time  = float(columns[0])
+    frequency = float(columns[1])
+    timelist.append(time)
+    freqlist.append(frequency)
 
+# print(timelist)
+# print(freqlist)
+
+fig, ax = plt.subplots(nrows = 1, ncols = 1)
+ax.plot(timelist, freqlist, ".")
+ax.set(xlabel = "TIME", ylabel = "FREQUENCIES")
+fig.tight_layout()
+plt.show()
