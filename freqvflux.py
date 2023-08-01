deda020c3d6fa92b3e5fbc0c339a9aaa285e771b
Casey Feng <caseyyfeng@gmail.com>
2023-08-01 11:51:26 -0600

    updated frequency and flux plots

--- freqvflux.py
+++ freqvflux.py
@@ -12,27 +12,60 @@ pathname = Path("/opt/data/mirror")
 
 fermifilename = list(pathname.glob(fermipath))[0]
 swiftfilename = list(pathname.glob(swiftflux))[0]
-fdata, fheader = fits.getdata(fermifilename,  header=True)
+fdata = fits.open(fermifilename)
 sdata, sheader = fits.getdata(swiftfilename,  header=True)
 tzero = swiftbat.met2mjd(swiftbat.string2met('2017-09-04T00:00:00'))
 print(tzero)
+fdata = fdata[2].data
+print(len(fdata['BARYTIME']))
+pass
 
-# print out all the time gaps and the durations
-for datasegment in np.split(data, splitlocs):
-    # time of spacecraft, not always accurate because of clock error
-    starttime = swiftbat.met2datetime(datasegment[timecolname][0])
-    duration = datasegment[timecolname].ptp()
-    print(f"{starttime:%Y-%m-%dT%H:%M:%S} + {duration:5.10f} seconds to the end of the block")   
+ratechangefreqs = []
+ratechangesflux = []
+ratechangefflux = []
+ftimes = []
+fmidtimes = []
+# smidtimes = []
 
-    # refind the amount of datapoints afte cutting off the first minute (BAT is still adjusting the first min)
-    n = len(datasegment) - int(60 / tb)
+for i in np.arange(0, len(fdata['BARYTIME']) - 1):
+    # get rate of change in frequency
+    changeftime = fdata['BARYTIME'][i+1] - fdata['BARYTIME'][i]
+    changeffreq = fdata['FREQUENCY'][i+1] - fdata['FREQUENCY'][i]
+    ratefchange = changeffreq / changeftime
+    ratechangefreqs.append(ratefchange)
 
-    # Trim to the last n values
-    datasegment = datasegment[-n:]
-    duration = datasegment[timecolname].ptp()
-    print(f"{duration:.3f} seconds after trimming")
+    changestime = sdata['TIME'][i+1] - sdata['TIME'][i]
+    changesfreq = sdata['RATE'][i+1] - sdata['RATE'][i]
+    rateschange = changesfreq / changestime
+    ratechangesflux.append(rateschange)
+
+    # get rate of change in flux
+    changeflux = fdata['AMPLITUDE'][i+1] - fdata['AMPLITUDE'][i]
+    rateflux = changeflux / changeftime
+    ratechangefflux.append(rateflux)
+    ftimes.append(changeftime)
+    fmidtimes.append(fdata['BARYTIME'][i] + changeftime / 2)
+    # smidtimes.append(sdata['TIME'][i] + changestime / 2)
+
+print(len(smidtimes))
+pass
+
+ffluxoverfreq = []
+sfluxoverfreq = []
+
+for i in np.arange(0,249):
+    ffluxoverfreq.append(ratechangefflux[i] / ratechangefreqs[i]) 
+    sfluxoverfreq.append(ratechangesflux[i] / ratechangefreqs[i])
 
 
+fig, axes = plt.subplots(nrows = 2, ncols = 1)
+axes[0].plot(ratechangefreqs, ratechangefflux, ".")
+axes[0].plot(ratechangefreqs, ratechangesflux, ".")
+axes[1].plot(fmidtimes, ffluxoverfreq, ".")
+axes[1].plot(fmidtimes, sfluxoverfreq, ".")
 
-    df= -.12/600 -.9/12000 - .1/30000
+axes[0].set(xlabel = 'Change in Frequency', ylabel = 'Change in Flux')
+axes[0].legend(['fdata', 'sdata'])
+axes[1].set(ylabel = 'Change in Flux over Change in Frequency', xlabel = 'Time')
+
 plt.show()
